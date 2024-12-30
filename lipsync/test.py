6e786760bc954cfd2a2be76dbf38ca5eb10dc527
Nathan Whitehead <nwhitehe@gmail.com>
2024-12-30 11:59:12 -0700

    Demo with lisa and rhubarb. Not great.

--- lipsync/test.py
+++ lipsync/test.py
@@ -5,29 +5,36 @@ import random
 import p2v
 
 def main():
-    data = json.loads(open('data.json').read())
-    phonemes = []
+    data = json.loads(open('test2.json').read())
     screen = pg.display.set_mode((1024, 768))
     clock = pg.time.Clock()
     talk = pg.mixer.music.load('test2.ogg')
-    visemes = ['neutral', 'blink', 'ah', 'consonant', 'eee', 'oh', 'w', 'm', 'ch']
     done = False
     playing = False
     start_time = 0
-    imgs = [pg.image.load(f'{v}.png') for v in visemes]
-    state = 0
+    state = 'X'
     last_transition_time = 0
-    next_state = 0
-
-    phoneme_list  = [ph['phoneme'] for ph in data['phonemes']]
-    phoneme_times  = [ph['time'] for ph in data['phonemes']]
-    phoneme_list = phoneme_list[:-1]
-    viseme_list, viseme_times = p2v.to_visemes(phoneme_list, phoneme_times)
-    delta = -80
+    next_state = 'X'
+    cue_map = {
+        'X': 'lisa-X',
+        'A': 'lisa-A',
+        'B': 'lisa-B',
+        'C': 'lisa-C',
+        'D': 'lisa-D',
+        'E': 'lisa-E',
+        'F': 'lisa-F',
+        'G': 'lisa-G',
+        'H': 'lisa-H',
+    }
+    imgs = {
+        key: pg.image.load(f'{cue_map[key]}.png')
+        for key in cue_map
+    }
+    delta = 0
     visemes = [ {
-        'viseme': v,
-        'time': t + delta,
-    } for v, t in zip(viseme_list, viseme_times)]
+        'viseme': cue['value'],
+        'time': cue['start'] * 1000 + delta,
+    } for cue in data['mouthCues']]
     original_visemes = visemes[:]
     print(visemes)
 
@@ -40,27 +47,9 @@ def main():
                 viseme = visemes.pop(0)
                 sys.stdout.write(f"{viseme['viseme']} ")
                 sys.stdout.flush()
-                next_state = -1
-                v = viseme['viseme']
-                if v in ['sil']:
-                    next_state = 0
-                if v in ['aa', 'E']:
-                    next_state = 2
-                if v in ['DD', 'nn', 'kk', 'SS', 'RR']:
-                    next_state = 3
-                if v in ['I']:
-                    next_state = 4
-                if v in ['U']:
-                    next_state = 6
-                if v in ['PP', 'FF']:
-                    next_state = 0
-                if v in ['CH']:
-                    next_state = 8
-                if next_state == -1:
-                    print(v)
-                    next_state = 0
+                next_state = viseme['viseme']
             if len(visemes) == 0:
-                next_state = 0
+                next_state = 'X'
             if time - last_transition_time > 100:
                 state = next_state
                 last_transition_time = time
@@ -73,9 +62,9 @@ def main():
                 playing = True
                 pg.mixer.music.play()
                 visemes = original_visemes[:]
-                state = 0
+                state = 'X'
                 last_transition_time = 0
-                next_state = 0
+                next_state = 'X'
 
 
 if __name__ == '__main__':
